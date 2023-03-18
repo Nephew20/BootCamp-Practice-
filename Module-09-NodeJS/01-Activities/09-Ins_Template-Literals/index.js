cb54b166e997fe1ebd90b64132b5aba0f445a495
Nephew20 <cofieldk20@gmail.com>
2023-03-18 18:12:14 -0400

    added module 09

---
+++ Module-09-NodeJS/01-Activities/09-Ins_Template-Literals/index.js
@@ -0,0 +1,23 @@
+//tildas keep line breaks 
+
+let myString = `hey 
+
+
+
+what's up`
+
+console.log(myString)
+
+// const arya = {
+//   first: "Arya",
+//   last: "Stark",
+//   origin: "Winterfell",
+//   allegiance: "House Stark"
+// };
+
+// const greeting = `My name is ${arya.first}!
+// I am loyal to ${arya.allegiance}.`;
+
+// console.log(greeting); // prints
+// // My name is Arya!
+// // I am loyal to House Stark.
